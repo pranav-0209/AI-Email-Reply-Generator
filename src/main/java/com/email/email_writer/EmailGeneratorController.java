0167baad20e56a2e16cb866461b0af3e4ecc2e16
pranav-0209 <pnarvekar0209@gmail.com>
2025-09-28 01:57:12 +0530

    Cross-origin added

--- src/main/java/com/email/email_writer/EmailGeneratorController.java
+++ src/main/java/com/email/email_writer/EmailGeneratorController.java
@@ -2,14 +2,12 @@ package com.email.email_writer;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
